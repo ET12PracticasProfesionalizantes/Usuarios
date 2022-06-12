d48a9a86cf7eb06996f0b0352d46c8e6bbd8b59a
jvelazquez <jonathanvgms@gmail.com>
2022-06-11 22:22:58 -0300

    Add logging

--- src/models/index.js
+++ src/models/index.js
@@ -7,12 +7,15 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-
+console.log(env)
+console.log(config)
 let sequelize;
 if (config.use_env_variable) {
+    console.log('use_env_variable')
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
 } else {
+    console.log('no use_env_variable')
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
