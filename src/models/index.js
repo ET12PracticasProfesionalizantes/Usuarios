4cf6bc7feae5300979ee4a49f1d3730e6fef9947
jvelazquez <jonathanvgms@gmail.com>
2022-06-11 21:41:09 -0300

    Fix config file

--- src/models/index.js
+++ src/models/index.js
@@ -13,7 +13,9 @@ console.log(env)
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    console.log('por aca')
 } else {
+    console.log('por aca2')
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
